e459ed227d8ecfbd742d77ead2b2c94195478acd
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-19 12:22:11 +0530

    class3 day3 demo2

---
+++ Day3/Day3/Demo2/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Demo2
+{
+    internal class Class3
+    {
+        public void show()
+        {
+            Console.WriteLine("I am coming from Class3 of Demo2");
+        }
+    }
+}
