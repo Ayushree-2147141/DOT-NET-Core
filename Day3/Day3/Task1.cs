fdbf7a71428a05651548df4352fc4e2a6450c173
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-21 09:04:48 +0530

    Day 3 program task

---
+++ Day3/Day3/Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Shape
+    {
+        protected Location c;
+
+        public string ToString()
+        {
+            return string.Empty;
+        }
+
+        public double Area()
+        {
+            return 0.000;
+        }
+
+        public double Perimeter()
+        {
+            return 0.000;
+        }
+    }
+
+    public class Location
+    {
+        private double x, y;
+    }
+
+    public class Rectangle : Shape
+    {
+        protected double side1, side2;
+    }
+
+    public class Circle : Shape
+    {
+        protected double radius;
+    }
+
+    internal class Task1
+    {
+    }
+}
---
+++ Day3/Day3/Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Person
+    {
+        public string Name;
+
+        public Person(string username) 
+        {
+            Name = username;
+        }
+
+        public override string ToString()
+        {
+            return "Hello! My name is " + Name;
+        }
+
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+    }
+    internal class Task2
+    {
+        public static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Enter name : " + (i + 1));
+                persons[i] = new Person(Console.ReadLine());
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+    }
+}
