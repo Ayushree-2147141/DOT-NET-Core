19a6b8cd477b067577f74c9f6b819a9af3ea1e1c
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-21 09:01:36 +0530

    program day 4

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day4
+{
+    //constructor and destructor
+    //constructor : to create memory block
+
+    //desctructor : destroys the object, when execution is finished
+    class Sample
+    {
+        public int MyProperty { get; set; } //prop : creating property
+        public Sample() //ctor
+        {
+            
+        }
+        ~Sample() { }
+    }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Sample s = new Sample();
+        }
+    }
+}
