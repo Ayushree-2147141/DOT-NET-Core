5ceca15373fdd60f21bc6ec6476814f7d79040ba
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-27 11:22:04 +0530

    API for get and post

---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using WebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        CourseDbContext db = new CourseDbContext();
+
+        [HttpGet]
+        public IEnumerable<Course> Index()
+        {
+            return db.Courses;
+        }
+        [HttpPost]
+        public IActionResult PostBooks(Course c)
+        {
+            db.Courses.Add(c);
+            db.SaveChanges();
+            return Ok("Courses added successfully");
+        }
+    }
+}
