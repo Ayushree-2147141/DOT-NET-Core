7124a9d929fd57d6e2d42488e29da13e8abb6180
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-10-03 12:04:25 +0530

    Added books

--- DemoWebApp/DemoWebApp/Controllers/HomeController.cs
+++ DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DemoWebApp.Models;
+using System.Text.Json.Serialization;
+
+using Newtonsoft.Json;
 
 namespace DemoWebApp.Controllers
 {
@@ -6,7 +10,21 @@ namespace DemoWebApp.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Books> booklist = new List<Books>();
+
+            //HttpClient client = new HttpClient();
+
+            //dispose the object when not in use
+            using(HttpClient client = new HttpClient())
+            {
+                // type of request
+                // api
+                // type of response
+
+                var data = client.GetAsync("https://localhost:7267/api/Home").Result.Content.ReadAsStringAsync().Result;
+                booklist = JsonConvert.DeserializeObject<IEnumerable<Books>>(data);
+            }
+            return View(booklist);
         }
     }
 }
---
+++ DemoWebApp/DemoWebApp/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoWebApp.Models
+{
+    public class Books
+    {
+        public int id {  get; set; }
+        public string courseName { get; set; }
+        public string bookUrl {  get; set; }
+    }
+}
